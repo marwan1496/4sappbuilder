430f94d15da679d88a60aef7bef31c8c86669a44
Marwan <marwanshokry@rocketmail.com>
2023-04-09 17:47:18 +0200

    remove init files

--- lib/src/position/PositionResponse.dart
+++
@@ -1,6 +0,0 @@
-import 'package:geolocator/geolocator.dart';
-
-class PositionResponse {
-  late Position position;
-  bool timedOut = false;
-}
--- lib/src/themes/UIColors.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter/material.dart';
- 
-class UIColors {
-  static Color primary = Color(0xFF1d55f3);
-}
